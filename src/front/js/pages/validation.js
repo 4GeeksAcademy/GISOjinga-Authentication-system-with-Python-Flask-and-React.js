3e86ed8dde83fe469895c20e9fdd40542531e4e4
GISOjinga <103523730+GISOjinga@users.noreply.github.com>
2023-09-07 03:55:44 +0000

    Fixed Valid

--- src/front/js/pages/validation.js
+++ src/front/js/pages/validation.js
@@ -1,25 +1,49 @@
-import React from "react";
-import { useAuth } from "../AuthContext"; // Import the context
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../AuthContext"; // Assuming AuthContext is in 'context' directory
 
 const Validation = () => {
-  const { isLoggedIn, setIsLoggedIn } = useAuth(); // Use the context
+    const [data, setData] = useState(null);
+    const { setIsLoggedIn } = useAuth(); // Use the Auth context
+    const navigate = useNavigate();
 
-  setIsLoggedIn(true); // Set the state to true
+    useEffect(() => {
+        // Function to fetch data from the /private route
+        const fetchData = async () => {
+            try {
+                const result = await axios.get("/private", {
+                    headers: {
+                        Authorization: "Bearer " + localStorage.getItem("token"), // Replace with how you store the token
+                    },
+                });
+                
+                if (result.data.success) {
+                    setData(result.data.data); // Assuming server sends confidential data
+                }
+            } catch (error) {
+                // If an error occurs (e.g., token invalid, server error), log the user out
+                setIsLoggedIn(false);
+                localStorage.removeItem("token");
+                navigate("/login");
+            }
+        };
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <h1>Validation Page</h1>
+        fetchData();
+    }, [setIsLoggedIn, navigate]);
+
+    return (
+        <div>
+            {data ? (
+                <>
+                    <h1>Validation Successful</h1>
+                    <p>Your confidential data: {data}</p>
+                </>
+            ) : (
+                <h1>Validating...</h1>
+            )}
         </div>
-      </div>
-      <div className="row">
-        <div className="col-12">
-          <p>You have successfully logged in!</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Validation;
